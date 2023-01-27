6f59dff3a37667708fa51f2e44f6e3334291ac0a
j-sudell <17751176+j-sudell@users.noreply.github.com>
2023-01-27 12:04:24 +0000

    Github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'www.jsudell.com',
   integrations: [mdx(), sitemap()]
 });
